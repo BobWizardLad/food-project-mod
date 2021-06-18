09bd7f2519f4bfe63af59e8a3feeb9aaed8af066
BobWizardLad <49493813+BobWizardLad@users.noreply.github.com>
2021-06-18 12:35:18 -0500

    Project files directory organized
    Directory for com.bobwizardlad.brewprojectmod.BrewProjectMod.java exists
    
    Setup runs correctly, next moves are to implement features from trello

---
+++ src/main/java/com/bobwizardlad/brewprojectmod/BrewProjectMod.java
@@ -0,0 +1,11 @@
+package com.bobwizardlad.brewprojectmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BrewProjectMod implements ModInitializer{
+    @Override
+    public void onInitialize() {
+        // This block runs when the game reaches a mod-loading state. TODO -- Call registry methods
+
+    }
+}
