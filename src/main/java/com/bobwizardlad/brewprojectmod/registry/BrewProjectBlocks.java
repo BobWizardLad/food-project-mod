6bb3381929fd22941b05a903c37a9294908d724a
BobWizardLad <49493813+BobWizardLad@users.noreply.github.com>
2021-06-21 12:18:12 -0500

    Tea Bush Part 01
    Implemented plants subpackage for special plant blocks. Added GreenTeaBush and started on implementation of registry and code

--- src/main/java/com/bobwizardlad/brewprojectmod/registry/BrewProjectBlocks.java
+++ src/main/java/com/bobwizardlad/brewprojectmod/registry/BrewProjectBlocks.java
@@ -1,6 +1,15 @@
 package com.bobwizardlad.brewprojectmod.registry;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
+
 public class BrewProjectBlocks {
+
+    // Blocks
+    public static final Block GREEN_TEA_BLOCK = new Block(FabricBlockSettings.of(Material.PLANT).ticksRandomly().noCollision().strength(0.0f, 0.0f).sounds(BlockSoundGroup.CROP).nonOpaque());
+    
 
     public static void registerBlocks() {
         // Call the registry to register blocks here
--- src/main/java/com/bobwizardlad/brewprojectmod/registry/BrewProjectItems.java
+++ src/main/java/com/bobwizardlad/brewprojectmod/registry/BrewProjectItems.java
@@ -6,7 +6,12 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BrewProjectItems {
-    public static final Item GREEN_TEA_LEAF = new Item(new Item.Settings().group(BrewProjectMod.BREW_ITEM_GROUP));
+    // Items
+    public static final Item GREEN_TEA_LEAF = new Item(new Item.Settings().group(BrewProjectMod.BREW_ITEM_GROUP).);
+    //SWEET_BERRIES = register((String)"sweet_berries", (Item)(new AliasedBlockItem(Blocks.SWEET_BERRY_BUSH, (new Settings()).group(ItemGroup.FOOD).food(FoodComponents.SWEET_BERRIES))));
+
+    // BlockItems
+
 
     public static void registerItems() {
         // Call the registry to register items here
