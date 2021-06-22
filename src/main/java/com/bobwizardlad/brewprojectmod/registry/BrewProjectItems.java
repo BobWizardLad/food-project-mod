1cb0bbf9104cf140565747daf8eb2c5f7ddf4bc6
BobWizardLad <49493813+BobWizardLad@users.noreply.github.com>
2021-06-22 11:41:59 -0500

    Semifermented, Fermented, Mushy Tea Leaves
    Added all smelted variants of tea leaves as registered, textured items.

--- src/main/java/com/bobwizardlad/brewprojectmod/registry/BrewProjectItems.java
+++ src/main/java/com/bobwizardlad/brewprojectmod/registry/BrewProjectItems.java
@@ -9,6 +9,9 @@ import net.minecraft.util.registry.Registry;
 public class BrewProjectItems {
     // Items
     public static final Item GREEN_TEA_LEAF = new GreenTeaLeaf(BrewProjectBlocks.GREEN_TEA_BUSH, (new Item.Settings()).group(BrewProjectMod.BREW_ITEM_GROUP));
+    public static final Item SEMIFERMENTED_TEA_LEAF = new Item(new Item.Settings().group(BrewProjectMod.BREW_ITEM_GROUP));
+    public static final Item FERMENTED_TEA_LEAF = new Item(new Item.Settings().group(BrewProjectMod.BREW_ITEM_GROUP));
+    public static final Item MUSHY_TEA_LEAF = new Item(new Item.Settings().group(BrewProjectMod.BREW_ITEM_GROUP));
 
     // BlockItems
 
@@ -16,5 +19,8 @@ public class BrewProjectItems {
     public static void registerItems() {
         // Call the registry to register items here
         Registry.register(Registry.ITEM, new Identifier(BrewProjectMod.MOD_ID, "green_tea_leaf"), GREEN_TEA_LEAF);
+        Registry.register(Registry.ITEM, new Identifier(BrewProjectMod.MOD_ID, "semifermented_tea_leaf"), SEMIFERMENTED_TEA_LEAF);
+        Registry.register(Registry.ITEM, new Identifier(BrewProjectMod.MOD_ID, "fermented_tea_leaf"), FERMENTED_TEA_LEAF);
+        Registry.register(Registry.ITEM, new Identifier(BrewProjectMod.MOD_ID, "mushy_tea_leaf"), MUSHY_TEA_LEAF);
     }
 }
