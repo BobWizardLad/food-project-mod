0a2536984d203cf7bc28091a0541dabb9f2c34c7
BobWizardLad <49493813+BobWizardLad@users.noreply.github.com>
2021-06-22 12:44:16 -0500

    Mug.
    Implemented Mug

--- src/main/java/com/bobwizardlad/brewprojectmod/registry/BrewProjectItems.java
+++ src/main/java/com/bobwizardlad/brewprojectmod/registry/BrewProjectItems.java
@@ -12,6 +12,7 @@ public class BrewProjectItems {
     public static final Item SEMIFERMENTED_TEA_LEAF = new Item(new Item.Settings().group(BrewProjectMod.BREW_ITEM_GROUP));
     public static final Item FERMENTED_TEA_LEAF = new Item(new Item.Settings().group(BrewProjectMod.BREW_ITEM_GROUP));
     public static final Item MUSHY_TEA_LEAF = new Item(new Item.Settings().group(BrewProjectMod.BREW_ITEM_GROUP));
+    public static final Item MUG = new Item(new Item.Settings().group(BrewProjectMod.BREW_ITEM_GROUP));
 
     // BlockItems
 
@@ -22,5 +23,6 @@ public class BrewProjectItems {
         Registry.register(Registry.ITEM, new Identifier(BrewProjectMod.MOD_ID, "semifermented_tea_leaf"), SEMIFERMENTED_TEA_LEAF);
         Registry.register(Registry.ITEM, new Identifier(BrewProjectMod.MOD_ID, "fermented_tea_leaf"), FERMENTED_TEA_LEAF);
         Registry.register(Registry.ITEM, new Identifier(BrewProjectMod.MOD_ID, "mushy_tea_leaf"), MUSHY_TEA_LEAF);
+        Registry.register(Registry.ITEM, new Identifier(BrewProjectMod.MOD_ID, "mug"), MUG);
     }
 }
